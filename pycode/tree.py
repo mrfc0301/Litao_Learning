126db95a37f79beedc14885a93faeb20bcb16746
litao <1312863760@qq.com>
2020-10-25 23:16:18 +0800

    用树模拟文件系统

---
+++ pycode/tree.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+"""
+作者：李涛
+日期：2020年10月24日
+"""
+class Node:
+    def __init__(self, name, type='dir'):
+        self.name = name
+        self.type = type
+        self.children = []
+        self.parent = None
+
+    def __repr__(self):
+        return self.name
+
+class FileSystemTree:
+    def __init__(self):
+        self.root = Node('/')
+        self.now = self.root
+
+    def mkdir(self, name):
+        if name[-1] != '/':
+            name += '/'
+        node = Node(name)
+        self.now.children.append(node)
+        node.parent = self.now
+
+    def ls(self):
+        return self.now.children
+
+    def cd(self, name):
+        if name[-1] != '/':
+            name += '/'
+        if name == '../':
+            self.now = self.now.parent
+            return
+        for child in self.now.children:
+            if child.name == name:
+                self.now = child
+                return
+        raise  ValueError('invalid dir')
+
+tree = FileSystemTree()
+tree.mkdir('var/')
+tree.mkdir('bin/')
+tree.mkdir('usr/')
+
+tree.cd('bin/')
+tree.mkdir('python/')
+tree.cd('../')
+
+print(tree.ls())
