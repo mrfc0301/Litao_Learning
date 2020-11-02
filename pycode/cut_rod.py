0da5cccdcf58c1619ead088d856f28014fc7ad71
litao <1312863760@qq.com>
2020-11-02 17:46:14 +0800

    钢条切割问题

--- pycode/cut_rod.py
+++ pycode/cut_rod.py
@@ -44,6 +44,30 @@ def cut_rod_dp(p, n):
         r.append(res)
     return r[n]
 
+def cut_rod_extend(p, n):
+    r = [0]
+    s = [0]
+    for i in range(1, n+1):
+        res_r = 0
+        res_s = 0
+        for j in range(1, i+1):
+            if p[j] + r[i-j] > res_r:
+                res_r = p[j] + r[i-j]
+                res_s = j
+        r.append(res_r)
+        s.append(res_s)
+    return r[n], s
+
+def cut_rod_solution(p, n):
+    r, s = cut_rod_extend(p, n)
+    ans = []
+    while n > 0:
+        ans.append(s[n])
+        n -= s[n]
+    return ans
+
+print(cut_rod_solution(p, 7))
+
 print(c1(p, 15))
 print(c2(p, 15))
 print(cut_rod_dp(p, 15))
